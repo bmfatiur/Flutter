bbe7e63b47eaa2b68f04544dc57d28b2f70939d1
bmfatiur <bmfr@bm.mars>
2021-11-23 18:43:55 +0600

    Day 12 Splash Screen

---
+++ Day-12 (22-11-2021)/Splash Screen.dart
@@ -0,0 +1,63 @@
+/*
+img Link 01: https://imgur.com/DSDMpHm
+img Link 02: https://imgur.com/MP2NcVm
+*/
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: splash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  _splashState createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlutterLogo(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Splash Screen'),
+      ),
+    );
+  }
+}
+
